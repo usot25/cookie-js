30d8568fe200a3627dc8f9f31a7754404818824b
Utsav <utsav.singh@onetrust.com>
2026-06-17 16:00:30 +0530

    js bundle sample test

---
+++ third-party-bundle.js
@@ -0,0 +1,21 @@
+(function () {
+
+    function loadThirdPartyFrame() {
+        var iframe = document.createElement("iframe");
+
+        iframe.src = "https://usot25.github.io/cookie-js/cookie_script_3rd_party.html";
+        iframe.style.display = "none";
+        iframe.width = "0";
+        iframe.height = "0";
+
+        document.body.appendChild(iframe);
+    }
+
+    // Delay like real trackers (after page load)
+    if (document.readyState === "complete") {
+        loadThirdPartyFrame();
+    } else {
+        window.addEventListener("load", loadThirdPartyFrame);
+    }
+
+})();
